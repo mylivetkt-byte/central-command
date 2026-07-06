1799cf426af9b3b0682c7117aacc87f67e0a85e0
mylivetkt-byte <mylivetkt-byte@users.noreply.github.com>
2026-07-06 14:14:44 -0500

    Fix: RLS permite company_id NULL (datos existentes) + revert HashRouter a BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -47,7 +47,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter>
+      <BrowserRouter>
         <AuthProvider>
           <CompanyProvider>
           <Routes>
@@ -84,7 +84,7 @@ const App = () => (
           </Routes>
           </CompanyProvider>
         </AuthProvider>
-        </HashRouter>
+        </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
